74e51b31b99d5717704379c6951f3b8c0f736280
Matt Steiner <Metropolis.4488@gmail.com>
2020-08-25 21:50:18 -0400

    added HTML

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const startBtn = document.getElementById("start");
+
+startButton.addEventListener("click",startGame);
+
+function startGame() {
+    console.log("start");
+}
+
+// function setNextQuestion {
+
+// }
+
+// function selectAnswer {
+
+// }
